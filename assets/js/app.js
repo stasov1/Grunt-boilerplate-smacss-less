aadb006314aaa23fb6de138b5033bb44dbcd627c
stasov1 <stasov1@mail.ru>
2015-10-22 11:21:56 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,1 @@
-(function($) {
-  $.myJ = {
-    name: "My libs",
-    version: "1.0.0",
-    consoleReport: function() {
-      console.log("Hello!");
-    }
-  };
-})(window);
-myJ.consoleReport();
+
